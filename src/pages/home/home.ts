0ab6f83ae5880b5675a9a99a05ac4a372f80e24c
Wilfredo Montanez <Wilfredo Montanez>
2017-03-17 09:15:31 -0400

    implement main page GUI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  driver:any;
+  routes:any;
   constructor(public navCtrl: NavController) {
-    
-  }
-
+    this.driver={
+      driver_name:"Juan Alonso",
+      driver_route:"Route 1",
+      bus_name: "Bus X"
+    }
+    this.routes=[
+      {route_name:"Route 1"},
+      {route_name:"Route 2"},
+      {route_name:"Route 3"},
+      {route_name:"Route 4"},
+      {route_name:"Route 5"},
+      {route_name:"Route 6"},
+      {route_name:"Route 7"},
+      {route_name:"Route 8"}
+    ]
+      
+    }
 }
+
+
