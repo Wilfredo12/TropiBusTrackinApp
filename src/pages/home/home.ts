0fb23177a4b57d535bc969a1b4c91580e96edcf1
Wilfredo Montanez <Wilfredo Montanez>
2017-03-31 20:45:21 -0400

    more documentation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,32 +20,32 @@ export class HomePage {
           public alertCtrl:AlertController) {
     //todo cuando haga el get de driver inicialzar ruta y estado con base datos
     
-    // this.driver={
-    //   driver_name:"Juan Alonso",
-    //   driver_route:"Route 1",
-    //   bus_name: "Bus X"
-    // }
           
   }
+  //this method runs when the page is initialize
   ngOnInit(){
-
+  
+  //loading circle is presented until information from the driver is fetched
   let loading = this.loading.create({
     content: 'Please wait...'
   });
 
   loading.present(loading);
+    //loggedIn true to indicate driver is currently logged in
     this.loggedIn=true;
     this.driver={}
     this.driver_ID=Number(localStorage.getItem("userID"));
 
+    //if there is no driver id push back to login
     if(this.driver_ID==null){
       this.navCtrl.setRoot(LoginPage);
     }
-    
+    //get route id and route name from server
     this.infoService.getRoutes().subscribe(response =>{
         this.routes=response;
         console.log(response)
     })
+    //get driver info from server
     this.infoService.getComplete_Bus_Driver_Info(this.driver_ID).subscribe(response=>{
       console.log(response)
       this.driver=response;
@@ -54,15 +54,15 @@ export class HomePage {
       this.locationCycle();
     }
     else{
+      //if driver has not been assigned a bus
       this.presentAlert("You have not been assigned a bus","Contact Administrator")
     }
     })
     
     
   }
-  loadingInit(message){
-    
-  }
+  //this method is use to either update bus status or bus route
+  //it present a toast message when update is successfully completed
   update(){
     let loading = this.loading.create({
     content: 'Updating...'
@@ -75,61 +75,72 @@ export class HomePage {
       this.presentToast("Succesfully Updated")
     },err => {
         if(err.status==0){
-          this.presentAlert("No Internet Connection","Please establish a connection and try again");
+          this.presentAlert("Error Connecting to Server","Please establish a connection and try again");
         }  
     });
     this.route=null;
     this.status=null;
     
   }
+  //this method sends the location of the bus to the server
   locationCycle(){
     Geolocation.getCurrentPosition().then((myposition) => {
-      //check de internet
-      //enviar id de driver
+      
       let locationInfo= {driver_id:this.driver_ID,lat:myposition.coords.latitude,lng: myposition.coords.longitude}
       this.locationService.sendLocation(locationInfo).subscribe(success=>{
         
           },err => {
             if(err.status==0){
+              //if there is no connection to server present alert
               this.presentAlert("No Internet Connection","Please establish a connection and try again");
               
             }
             
         });
        }, (err) => {
+         //if location is not enable present alert
          this.presentAlert('Location not enable','Please go to location settings and enable location')
     })
+    //run location cycle every 5 seconds if driver is still logged in to the application
     setTimeout(()=>{
       if(this.loggedIn) this.locationCycle();
     },5000);
   }
+
+  //this method logs out driver from system
   logout(){
-    
+    //call log out to server
     this.auth.logout(this.driver_ID)
-    .subscribe(data=>{             
+    .subscribe(data=>{   
+      //logout successfull          
         if(data.success==1) { 
           localStorage.setItem("userID",null);
           this.loggedIn=false;
           this.presentToast("Succesfully Logged out")
           this.navCtrl.setRoot(LoginPage);          
         }
+        //present alert if logout is not successful
         else this.presentAlert('Could not logged out',"Try again");
     },err => {
             if(err.status==0){
+              //present alert is log out could not be process by server
               this.presentAlert("No Internet Connection","Please establish a connection and try again");
               
             }
             
         })
 }
-presentAlert(titl,subTitl){
+
+//this method is used to present the alert with the desired title and subtitle
+presentAlert(title,subTitle){
     let alert = this.alertCtrl.create({
-            title: titl,
-            subTitle: subTitl,
+            title: title,
+            subTitle: subTitle,
             buttons: ['Dismiss']
           });
     alert.present();
 }
+//this method is used to present the toast with the message value
 presentToast(message) {
   let toast = this.toastCtrl.create({
     message: message,
